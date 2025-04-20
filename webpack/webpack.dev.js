67ee0ac7ac7dae61aa40510489b35dc36e9c9112
Alexandr Tropinskiy <vaskaop@mail.ru>
2025-04-20 21:15:27 +0300

    Исправлен base url

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -5,12 +5,17 @@ module.exports = {
 	mode: 'development',
 	devtool: 'eval-source-map',
 	devServer: {
-		historyApiFallback: true,
+    historyApiFallback: {
+      index: '/burger-constructor/',  // Указываем правильный подкаталог для работы с React Router
+    },
 		static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
 		// compress: true, // это ускорит загрузку в режиме разработки
 		port: 3000, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
-		open: true, // сайт будет открываться сам при запуске npm run dev
+		open: '/burger-constructor/', // сайт будет открываться сам при запуске npm run dev
 		hot: true,
 	},
+	output: {
+    publicPath: '/burger-constructor/', // Указание на подкаталог
+  },
 	plugins: [new ReactRefreshWebpackPlugin()],
 };
